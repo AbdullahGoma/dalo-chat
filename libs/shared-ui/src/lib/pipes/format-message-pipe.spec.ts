91bebef7462b5d662eb65404559a10382e9996a8
AbdullahGomaa <abdullah.goma.010170@gmail.com>
2025-09-05 17:34:55 +0300

    fix format message pipe test

--- libs/shared-ui/src/lib/pipes/format-message-pipe.spec.ts
+++ libs/shared-ui/src/lib/pipes/format-message-pipe.spec.ts
@@ -1,8 +1,25 @@
+import { TestBed } from '@angular/core/testing';
+import { DomSanitizer } from '@angular/platform-browser';
 import { FormatMessagePipe } from './format-message-pipe';
 
 describe('FormatMessagePipe', () => {
-  it('create an instance', () => {
-    const pipe = new FormatMessagePipe();
+  let pipe: FormatMessagePipe;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [FormatMessagePipe],
+    });
+
+    pipe = TestBed.inject(FormatMessagePipe);
+  });
+
+  it('should create an instance', () => {
     expect(pipe).toBeTruthy();
+  });
+
+  it('should sanitize html', () => {
+    const sanitizer = TestBed.inject(DomSanitizer);
+    const safe = pipe.transform('<b>hello</b>');
+    expect(safe).toBeTruthy();
   });
 });
